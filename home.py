51c3415281f2d808dc490408dacbf7dc71f4a46a
Estairriol <84276546+Estairriol@users.noreply.github.com>
2022-12-07 17:25:28 +0000

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import tkinter as tk, sys, os, time, sqlite3 #not all of these are used yet but they are universal to the project and so are imported in all files
 
-class Home:
-	def __init__(self, window, manager):
-		self.window = window #this allows us to make changes to the window
+class Home(tk.Frame):
+	def __init__(self, manager):
+		tk.Frame.__init__(self, manager) #initialises the frame
 		self.manager = manager #this allows us to communicate to the manager in main
